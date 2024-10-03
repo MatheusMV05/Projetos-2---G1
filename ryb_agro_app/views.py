3e9100f9773f7ac930964fbbfb04194921412923
Vinícius Bernardo <vbs4@cesar.school>
2024-10-02 21:33:18 -0300

    troquei o nome home para landingPage na view

--- ryb_agro_app/views.py
+++ ryb_agro_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'ryb_agro_app/home.html')
+def landinPage(request):
+    return render(request, 'ryb_agro_app/landingPage.html')
 
 def login(request):
     return render(request, 'ryb_agro_app/login.html')
