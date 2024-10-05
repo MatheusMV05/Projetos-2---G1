a761986a128dc6c7fee9beef2d6b3dfaaa1538a6
Matheus Verissimo <matheusmverissimo820@gmail.com>
2024-10-05 11:04:50 -0300

    atualizei pastas com conflito

--- ryb_agro_app/urls.py
+++ ryb_agro_app/urls.py
@@ -1,6 +1,14 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), 
+   
+    path('', views.landingPage, name='landingPage'),  
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('login/', views.login, name='login'),
+    path('recuperar-senha/', views.recuperar_senha, name='recuperar-senha'),
+    path('redefine-senha/', views.redefine_senha, name='redefine-senha'),
+    path('trocar-senha/', views.trocar_senha, name='trocar-senha'),# URL raiz para a página inicial
 ]
--- ryb_agro_app/views.py
+++ ryb_agro_app/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landingPage(request):
+    return render(request, 'landingPage.html')
 
-def index(request):
-    return render(request, 'ryb_agro_app/index.html')
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def recuperar_senha(request):
+    return render(request, 'recuperar-senha.html')
+
+def redefine_senha(request):
+    return render(request, 'redefine-senha.html')
+
+def trocar_senha(request):
+    return render(request, 'trocar-senha.html')
