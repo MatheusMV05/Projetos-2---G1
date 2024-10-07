2bf1059d5701e851630d7035ac0f26cc5f0c9b99
Vinícius Marques <82516598+vinimarques7@users.noreply.github.com>
2024-10-07 17:43:07 -0300

    Atualização das views

--- ryb_agro_app/views.py
+++ ryb_agro_app/views.py
@@ -1,23 +1,89 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from .models import usuario
+from django.contrib.auth.models import User
 
-# Create your views here.
+
 def landingPage(request):
     return render(request, 'landingPage.html')
 
 def cadastro(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        confirmar_email = request.POST.get('confirmar_email')
+        telefone = request.POST.get('telefone')
+        password = request.POST.get('password')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if email != confirmar_email:
+            messages.error(request, 'Os emails não coincidem.')
+            return redirect('cadastro')
+
+     
+        if password != confirmar_senha:
+            messages.error(request, 'As senhas não coincidem.')
+            return redirect('cadastro')
+
+        
+        user = User.objects.create_user(username=email, email=email, password=password)
+        usuario = usuario(user=user, nome_completo=nome, telefone=telefone)
+        usuario.save()
+
+        messages.success(request, 'Cadastro realizado com sucesso. Agora você pode fazer login.')
+        return redirect('login')
     return render(request, 'cadastro.html')
 
 def dashboard(request):
     return render(request, 'dashboard.html')
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Email ou senha incorretos.')
+            return redirect('login')
     return render(request, 'login.html')
+
 
 def recuperar_senha(request):
     return render(request, 'recuperar-senha.html')
 
 def redefine_senha(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        messages.success(request, f'Link de redefinição enviado para {email}.')
+        return redirect('login')
     return render(request, 'redefine-senha.html')
 
+
 def trocar_senha(request):
+    if request.method == 'POST':
+        password = request.POST.get('password')
+        new_password = request.POST.get('new_password')
+        confirmar_password = request.POST.get('confirmar_password')
+
+        if new_password != confirmar_password:
+            messages.error(request, 'As senhas novas não coincidem.')
+            return redirect('trocar_senha')
+
+        user = request.user
+        if not user.check_password(password):
+            messages.error(request, 'A senha atual está incorreta.')
+            return redirect('trocar_senha')
+
+        user.set_password(new_password)
+        user.save()
+
+        messages.success(request, 'Sua senha foi alterada com sucesso.')
+        return redirect('login')
+
     return render(request, 'trocar-senha.html')
