fe1d56e3e8e7f8a99546fd39ffce5f7cde4b66bd
Matheus Verissimo <matheusmverissimo820@gmail.com>
2024-10-07 17:25:57 -0300

    Adicionei a tabela Usuario no models(senha,email e numero de telefone)

--- ryb_agro_app/models.py
+++ ryb_agro_app/models.py
@@ -1,1 +1,24 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
 
+class UsuarioManager(BaseUserManager):
+    def create_user(self, email, celular, password=None):
+        if not email:
+            raise ValueError("O usuário deve ter um email")
+        usuario = self.model(email=self.normalize_email(email), celular=celular)
+        usuario.set_password(password)
+        usuario.save(using=self._db)
+        return usuario
+
+class Usuario(AbstractBaseUser):
+    celular = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    senha = models.CharField(max_length=100)
+
+    objects = UsuarioManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['celular']
+
+    def __str__(self):
+        return self.email
