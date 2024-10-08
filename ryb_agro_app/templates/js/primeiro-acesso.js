1ffa29a300c27fd6377efa97c20a73b140aa276a
Cláudia <mcrcoa@cesar.school>
2024-10-08 06:36:42 -0300

    correção js cidades

--- ryb_agro_app/templates/js/primeiro-acesso.js
+++ ryb_agro_app/templates/js/primeiro-acesso.js
@@ -12,29 +12,43 @@ menu.addEventListener('click',()=>{
 const paisInput = document.getElementById('pais');
 const estadoInput = document.getElementById('estado');
 const estadosDatalist = document.getElementById('estados');
+const cidadeInput = document.getElementById('cidade');
+const cidadesDatalist = document.getElementById('cidades');
 
 paisInput.addEventListener('change', (e) => {
-  const paisSelecionado = e.target.value;
-  const estadosDoPais = getEstadosDoPais(paisSelecionado); 
-  estadosDatalist.innerHTML = ''; 
-  estadosDoPais.forEach((estado) => {
-    const option = document.createElement('option');
-    option.value = estado;
-    option.text = estado;
-    estadosDatalist.appendChild(option);
-  });
+    const paisSelecionado = e.target.value;
+    const estadosDoPais = getEstadosDoPais(paisSelecionado); 
+    estadosDatalist.innerHTML = ''; 
+    estadosDoPais.forEach((estado) => {
+        const option = document.createElement('option');
+        option.value = estado;
+        option.text = estado;
+        estadosDatalist.appendChild(option);
+    });
 });
 
 function getEstadosDoPais(pais) {
   const estados = {
-    'Brasil': ['Acre (AC)', 'Alagoas (AL)', 'Amapá (AP)', 'Amazonas (AM)', 'Bahia (BA)', 'Ceará (CE)', 'Distrito Federal (DF)', 'Espírito Santo (ES)', 'Goiás (GO)', 'Maranhão (MA)', 'Mato Grosso (MT)', 'Mato Grosso do Sul (MS)', 'Minas Gerais (MG)', 'Pará (PA)', 'Paraíba (PB)', 'Paraná (PR)', 'Pernambuco (PE)', 'Piauí (PI)', 'Rio de Janeiro (RJ)', 'Rio Grande do Norte (RN)', 'Rio Grande do Sul (RS)', 'Rondônia (RO)', 'Roraima (RR)', 'Santa Catarina (SC)', 'São Paulo (SP)', 'Sergipe (SE)', 'Tocantins (TO)'],
+    'Brasil': ['Acre (AC)', 'Alagoas (AL)', 'Amapá (AP)', 'Amazonas (AM)', 'Bahia (BA)', 'Ceará (CE)', 'Distrito Federal (DF)', 'Espírito Santo (ES)', 'Goiás (GO)', 'Maranhão (MA)', 'Mato Grosso (MT)', 'Mato Grosso do Sul (MS)', 'Minas Gerais (MG)', 'Pará (PA)', 'Paraíba (PB)', 'Paraná (PR)', 'Pernambuco (PE)', 'Piauí (PI)', 'Rio de Janeiro (RJ)', 'Rio Grande do Norte (RN)', 'Rio Grande do Sul (RS)', 'Rondônia (RO)', 'Roraima (RR)', 'Santa Catarina (SC)', 'São Paulo (SP)', 'Sergipe (SE)', 'Tocantins (TO)']
   };
   return estados[pais];
 }
 
+estadoInput.addEventListener('change', (e) => {
+    const estadoSelecionado = e.target.value;
+    const cidadesDoEstado = getCidadesDoEstado(estadoSelecionado); 
+    cidadesDatalist.innerHTML = ''; 
+    cidadesDoEstado.forEach((cidade) => {
+        const option = document.createElement('option');    
+        option.value = cidade;
+        option.text = cidade;
+        cidadesDatalist.appendChild(option);
+    });
+});
+
 function getCidadesDoEstado(estado) {
     const cidades = {
-      'Pernambuco (PE)': ['Carpina'],
+        'Pernambuco (PE)': ['Carpina']
     };
     return cidades[estado];
-  }
+}
