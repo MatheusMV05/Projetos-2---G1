489118b06fbc98f8d312bf2b8fb116850097080a
JuliaTBarros <162466823+JuliaTBarros@users.noreply.github.com>
2024-10-13 16:43:13 -0300

    adição de imagens para os cards de pesquisa

--- ryb_agro_app/static/js/add-planta.js
+++ ryb_agro_app/static/js/add-planta.js
@@ -9,25 +9,31 @@ menu.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
 })
 
+const cardContainer = document.querySelector(".card-container");
+
 const data = [
     {
-        title: "Abóbora"
+        title: "Abóbora",
+        image: cardContainer.dataset.aboboraImg
     },
     {
-        title: "Batata doce"
+        title: "Batata doce",
+        image: cardContainer.dataset.batataImg
     },
     {
-        title: "Cenoura"
+        title: "Cenoura",
+        image: cardContainer.dataset.cenouraImg
     },
     {
-        title: "Inhame"
+        title: "Inhame",
+        image: cardContainer.dataset.inhameImg
     },
     {
-        title: "Milho"
+        title: "Milho",
+        image: cardContainer.dataset.milhoImg
     },
 ];
 
-const cardContainer = document.querySelector(".card-container");
 const searchInput = document.querySelector("#searchInput");
 
 const displayData = data => {
@@ -36,6 +42,7 @@ const displayData = data => {
         cardContainer.innerHTML += `
             <div class="card">
                 <h3>${e.title}</h3>
+                <img src="${e.image}" alt="${e.title}">
             </div>
             `
     });
@@ -46,18 +53,18 @@ searchInput.addEventListener("keyup", (e) => {
     displayData(search);
 })
 
-window.addEventListener("load", displayData.bind(null,data))
+window.addEventListener("load", displayData.bind(null, data))
 
 const section = document.querySelector(".section"),
-SumbitPop = section.querySelector("#submit"),
-closeSubmit = section.querySelectorAll("#close");
- 
-SumbitPop.addEventListener("click" , () =>{
+    SumbitPop = section.querySelector("#submit"),
+    closeSubmit = section.querySelectorAll("#close");
+
+SumbitPop.addEventListener("click", () => {
     section.classList.add("show");
 });
 
 closeSubmit.forEach(cSbmt => {
-    cSbmt.addEventListener("click" , ()=>{
+    cSbmt.addEventListener("click", () => {
         section.classList.remove("show");
     })
 });
