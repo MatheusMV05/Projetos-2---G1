5b4ce109862319c142118f6cb54580b77ca5dee2
JuliaTBarros <162466823+JuliaTBarros@users.noreply.github.com>
2024-11-05 18:27:15 -0300

    comentários

--- ryb_agro_app/models.py
+++ ryb_agro_app/models.py
@@ -49,32 +49,44 @@ class Planta(models.Model):
 
 # Modelo de Cronograma para cada planta
 class Cronograma(models.Model):
+    # Associa o cronograma a uma planta específica
     planta = models.ForeignKey(Planta, on_delete=models.CASCADE, related_name='cronogramas')
+    
+    # Descrição geral do cronograma (opcional)
     descricao_geral = models.TextField(blank=True, null=True)
 
+    # Representação em string do cronograma, mostrando o nome da planta associada
     def __str__(self):
         return f'Cronograma para {self.planta.nome}'
 
 # Modelo de Etapas para cada cronograma
 class Etapa(models.Model):
+    # Associa a etapa a um cronograma específico
     cronograma = models.ForeignKey(Cronograma, on_delete=models.CASCADE, related_name='etapas')
+    
+    # Nome da etapa (ex.: "Plantio", "Irrigação")
     nome = models.CharField(max_length=100)
+    
+    # Quantos dias após o plantio essa etapa ocorre
     dias_após_plantio = models.IntegerField()
-    intervalo_dias = models.PositiveIntegerField(default=0)  # Novo campo
+    
+    # Intervalo de dias entre repetições da etapa (valor padrão é 0)
+    intervalo_dias = models.PositiveIntegerField(default=0)
+    
+    # Descrição detalhada da etapa
     descricao = models.TextField()
-    tipo_acao = models.CharField(
-        max_length=50,
-        choices=[
-            ('Preparo do Solo', 'Preparo do Solo'),
-            ('Plantio', 'Plantio'),
-            ('Irrigação', 'Irrigação'),
-            ('Inspeção', 'Inspeção'),
-            ('Biofertilização', 'Biofertilização'),
-            ('Controle de Pragas', 'Controle de Pragas'),
-            ('Colheita', 'Colheita')
-        ],
-        default='Plantio'
-    )
+    
+    # Tipo da ação a ser realizada, com opções predefinidas
+    tipo_acao = models.CharField(max_length=50, choices=[
+        ('Preparo do Solo', 'Preparo do Solo'),
+        ('Plantio', 'Plantio'),
+        ('Irrigação', 'Irrigação'),
+        ('Inspeção', 'Inspeção'),
+        ('Biofertilização', 'Biofertilização'),
+        ('Controle de Pragas', 'Controle de Pragas'),
+        ('Colheita', 'Colheita')
+    ], default='Plantio')
 
+    # Representação em string da etapa, mostrando o nome e os dias após o plantio
     def __str__(self):
         return f'{self.nome} ({self.dias_após_plantio} dias após plantio)'
