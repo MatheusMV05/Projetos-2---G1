a71c5b0e3c1e05efc026e3b1d5632852f7c0c2f4
Vinícius Bernardo <vbs4@cesar.school>
2024-11-26 20:45:04 -0300

    iniciei os testes automatizados

---
+++ cypress/e2e/registrar_demandas.cy.js
@@ -0,0 +1,45 @@
+Cypress.Commands.add('deleteAllUsers', () => {
+    cy.exec('python delete_users.py', { failOnNonZeroExit: false })
+});
+
+Cypress.Commands.add('cadastro', () => {
+    cy.deleteAllUsers()
+    cy.visit('/')
+    cy.get('.button').click()
+    cy.get('.register-link > a').click()
+    cy.get('#nome').type("luffy")
+    cy.get('#celular').type('987654321')
+    cy.get('#email').type('reidospiratas@gmail.com')
+    cy.get('#confirmar_email').type('reidospiratas@gmail.com')
+    cy.get('#password').type('carne')
+    cy.get('#confirmar_senha').type('carne')
+    cy.get('.submit').click()
+});
+
+describe('registrar demandas', () => {
+    it('registrar uma nova demanda', () => {
+        //steps do cenario1
+        cy.cadastro();
+    })
+
+    /*it('visualizar demandas', () => {
+        //steps do cenario2
+    })
+
+    it('editar demanda', () => {
+        //steps do cenario3
+    })
+    
+    it('filtrar demandas', () => {
+        //steps do cenario4
+    })
+    
+    it('excluir demanda', () => {
+        //steps do cenario5
+    })
+    
+    it('notificar demanda', () => {
+        //steps do cenario6
+    })*/
+
+})
