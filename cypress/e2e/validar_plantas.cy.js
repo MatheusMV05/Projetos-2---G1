95f71dab79b2a0ce939e7938a5cf4a3f64d193fb
Matheus Verissimo <matheusmverissimo820@gmail.com>
2024-11-29 04:36:06 -0300

    Refactor Cypress tests for user registration and plant validation; update user data and streamline sector registration process

--- cypress/e2e/validar_plantas.cy.js
+++ cypress/e2e/validar_plantas.cy.js
@@ -1,77 +1,72 @@
 Cypress.Commands.add('deleteAllUsers', () => {
-    cy.exec('python delete_users.py', { failOnNonZeroExit: false });
+    cy.exec('python delete_users.py', { failOnNonZeroExit: false })
 });
 
 Cypress.Commands.add('cadastro', () => {
-    cy.deleteAllUsers();
-    cy.visit('/');
-    cy.get('.button').click();
-    cy.get('.register-link > a').click();
-    cy.get('#nome').type('Ana');
-    cy.get('#celular').type('987654321');
-    cy.get('#email').type('aa@gmail.com');
-    cy.get('#confirmar_email').type('aa@gmail.com');
-    cy.get('#password').type('123');
-    cy.get('#confirmar_senha').type('123');
-    cy.get('.submit').click();
+    cy.deleteAllUsers()
+    cy.visit('/')
+    cy.get('.button').click()
+    cy.get('.register-link > a').click()
+    cy.get('#nome').type("Monkey D. Luffy")
+    cy.get('#celular').type('987654321')
+    cy.get('#email').type('reidospiratas@gmail.com')
+    cy.get('#confirmar_email').type('reidospiratas@gmail.com')
+    cy.get('#password').type('carne')
+    cy.get('#confirmar_senha').type('carne')
+    cy.get('.submit').click()
 });
 
-Cypress.Commands.add('cadastroSetores', () => {
-    cy.visit('/cadastrar_setores/');
-    cy.get('#numSetores').clear().type('2'); // Cadastra 2 setores
-    cy.get('button').contains('Gerar Campos para Setores').click();
-    cy.get('#canteirosSetor1').type('3'); // Canteiros no setor 1
-    cy.get('#canteirosSetor2').type('2'); // Canteiros no setor 2
-    cy.get('button[type="submit"]').click();
-
-    // Verifica redirecionamento após cadastro
-    cy.url().should('include', '/planta/');
+Cypress.Commands.add('setores', () => {
+    cy.get('#numSetores').type('2')
+    cy.get('.btn-secondary').click()
+    cy.get('#canteirosSetor1').type('3')
+    cy.get('#canteirosSetor2').type('5')
+    cy.get('.btn-primary').click()
 });
 
-Cypress.Commands.add('addPlanta', (planta, setor, canteiro, quantidade, frequencia) => {
-    cy.get(`[data-title="${planta}"]`).click();
-    cy.get('#selectCanteiro option')
-        .should('contain', `${setor} - ${setor} ${canteiro}`)
-        .then(($option) => {
-            const value = $option.val();
-            cy.get('#selectCanteiro').select(value);
-        });
-    cy.get('#harvestAmount').clear().type(quantidade);
-    cy.get('#harvestFrequency').select(frequencia);
-    cy.get('#addPlantButton').click();
-});
-
-describe('Validação de Plantas', () => {
-    beforeEach(() => {
-        cy.cadastro(); // Realiza cadastro de usuário
-        cy.cadastroSetores(); // Cadastra setores e canteiros
+Cypress.Commands.add('add_plantas', () => {
+    cy.get('[data-title="Abóbora"]').click()
+    cy.get('#selectCanteiro').should('exist')
+    cy.get('#selectCanteiro option').should('have.length.greaterThan', 0).contains('Setor 1 - Setor 1 Canteiro B')
+    .then(($option) => {
+        const value = $option.val();
+        cy.get('#selectCanteiro').select(value);
     });
-
-    it('Deve validar plantas compatíveis', () => {
-        cy.addPlanta('Abóbora', 'Setor 1', 'Canteiro B', '50', 'Mensalmente');
-        cy.addPlanta('Milho', 'Setor 2', 'Canteiro A', '30', 'Semanalmente');
-
-        cy.get('#saveAndContinueButton').click();
-
-        cy.on('window:alert', (str) => {
-            expect(str).to.equal('As plantas são compatíveis');
-        });
+    cy.get('#harvestAmount').type('50')
+    cy.get('#harvestFrequency').select('semanalmente')
+    cy.get('#addPlantButton').click()
+    cy.get('[data-title="Milho"]').click()
+    cy.get('#selectCanteiro option').should('have.length.greaterThan', 0).contains('Setor 2 - Setor 2 Canteiro C')
+    .then(($option) => {
+        const value = $option.val();
+        cy.get('#selectCanteiro').select(value);
+        
     });
-
-    it('Deve exibir erro para plantas incompatíveis', () => {
-        cy.addPlanta('Abóbora', 'Setor 1', 'Canteiro B', '50', 'Mensalmente');
-        cy.addPlanta('Cenoura', 'Setor 2', 'Canteiro A', '20', 'Semanalmente');
-
-        cy.get('#saveAndContinueButton').click();
-
-        cy.on('window:alert', (str) => {
-            expect(str).to.equal('As plantas cadastradas são incompatíveis');
-        });
-
-        cy.get('#plantListContainer')
-            .should('be.visible')
-            .and('contain', 'Abóbora')
-            .and('contain', 'Cenoura');
-    });
+    cy.get('#harvestAmount').type('30')
+    cy.get('#harvestFrequency').select('uma vez')
+    cy.get('#addPlantButton').click()
+    cy.get('#saveAndContinueButton').click()
 });
+
+describe('Plantas', () => {
+    it('Plantas compativeis', () => {
+        //steps do cenario1
+        cy.cadastro()
+        cy.setores()
+        cy.add_plantas()
+
+        })
+
+    it('Plantas incopativeis', () => {
+        //steps do cenario2
+        cy.cadastro()
+        cy.setores()
+        cy.add_plantas()
+        cy.get('.card-clickable > .card-body').click()
+        cy.get('.card-clima-atual').click({ force: true });
+
+        
+        });
+
+    })
 
